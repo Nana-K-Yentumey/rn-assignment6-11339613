7e5cd63d6db0e4621b7e5c9d955565f8e0273042
death-inspires-me <nana.esseney@gmail.com>
2024-07-02 21:22:22 +0000

    Refactored cart screen to integrate with homescreen for storage and viewing/removing list of items in the cart

--- CartScreen.js
+++ CartScreen.js
@@ -1,8 +1,7 @@
-// screens/CartScreen.js
-
 import React, { useState, useEffect } from 'react';
-import { View, Text, Button, FlatList } from 'react-native';
+import { View, Text, Button, FlatList, StyleSheet, Image, TouchableOpacity } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { Ionicons } from '@expo/vector-icons';
 
 const CartScreen = ({ navigation }) => {
   const [cart, setCart] = useState([]);
@@ -24,14 +23,21 @@ const CartScreen = ({ navigation }) => {
   }, []);
 
   return (
-    <View>
+    <View style={styles.container}>
       <FlatList
         data={cart}
         keyExtractor={(item) => item.id}
         renderItem={({ item }) => (
-          <View>
-            <Text>{item.name}</Text>
-            <Button title="Remove from Cart" onPress={() => removeFromCart(item)} />
+          <View style={styles.productContainer}>
+            <Image source={item.image} style={styles.productImage} />
+            <View style={styles.productDetails}>
+              <Text style={styles.productName}>{item.name}</Text>
+              <Text style={styles.productDescription}>{item.description}</Text>
+              <Text style={styles.productPrice}>{item.price}</Text>
+              <TouchableOpacity style={styles.removeButton} onPress={() => removeFromCart(item)}>
+                <Ionicons name="remove-circle" size={32} color="red" />
+              </TouchableOpacity>
+            </View>
           </View>
         )}
       />
@@ -40,3 +46,47 @@ const CartScreen = ({ navigation }) => {
 };
 
 export default CartScreen;
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+    padding: 20,
+  },
+  productContainer: {
+    flexDirection: 'row',
+    marginBottom: 20,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+    paddingBottom: 10,
+  },
+  productImage: {
+    width: 100,
+    height: 100,
+    resizeMode: 'contain',
+  },
+  productDetails: {
+    flex: 1,
+    marginLeft: 20,
+    justifyContent: 'center',
+  },
+  productName: {
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+  productDescription: {
+    fontSize: 14,
+    color: '#888',
+    marginTop: 5,
+  },
+  productPrice: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    marginTop: 5,
+    color: '#dd8560',
+  },
+  removeButton: {
+    marginTop: 10,
+    alignSelf: 'flex-start',
+  },
+});
--- HomeScreen.js
+++ HomeScreen.js
@@ -1,46 +1,47 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, Button, FlatList, StyleSheet, Image, ScrollView } from 'react-native';
+import { View, Text, FlatList, StyleSheet, Image, ScrollView, TouchableOpacity } from 'react-native';
 import AsyncStorage from '@react-native-async-storage/async-storage';
+import { Ionicons } from '@expo/vector-icons';
 
 const products = [
   {
     id: '1',
-    name: 'Product 1',
-    description: 'This is a great product.',
+    name: 'Office Wear',
+    description: 'Reversible angora cardigan',
     price: '$10.00',
-    image: require('./assets/dress1.png'), // Replace with your actual image paths
+    image: require('./assets/dress1.png'),
   },
   {
     id: '2',
-    name: 'Product 2',
+    name: 'Black',
     description: 'This product is even better.',
     price: '$20.00',
     image: require('./assets/dress2.png'),
   },
   {
     id: '3',
-    name: 'Product 3',
+    name: 'Church Wear',
     description: 'You will love this product.',
     price: '$30.00',
     image: require('./assets/dress3.png'),
   },
   {
     id: '4',
-    name: 'Product 3',
+    name: 'Lamerei',
     description: 'You will love this product.',
     price: '$30.00',
     image: require('./assets/dress4.png'),
   },
   {
     id: '5',
-    name: 'Product 3',
+    name: '21WN',
     description: 'You will love this product.',
     price: '$30.00',
     image: require('./assets/dress5.png'),
   },
   {
     id: '6',
-    name: 'Product 3',
+    name: 'Lopo',
     description: 'You will love this product.',
     price: '$30.00',
     image: require('./assets/dress6.png'),
@@ -52,8 +53,6 @@ const products = [
     price: '$30.00',
     image: require('./assets/dress7.png'),
   },
-
-
 ];
 
 const HomeScreen = ({ navigation }) => {
@@ -90,11 +89,15 @@ const HomeScreen = ({ navigation }) => {
           keyExtractor={(item) => item.id}
           renderItem={({ item }) => (
             <View style={styles.productContainer}>
-              <Image source={item.image} style={styles.productImage} />
+              <View style={styles.imageContainer}>
+                <Image source={item.image} style={styles.productImage} />
+                <TouchableOpacity style={styles.addButton} onPress={() => addToCart(item)}>
+                  <Ionicons name="add-circle" size={32} color="white" />
+                </TouchableOpacity>
+              </View>
               <Text style={styles.productName}>{item.name}</Text>
               <Text style={styles.productDescription}>{item.description}</Text>
               <Text style={styles.productPrice}>{item.price}</Text>
-              <Button title="Add to Cart" onPress={() => addToCart(item)} />
             </View>
           )}
           numColumns={2}
@@ -134,17 +137,23 @@ const styles = StyleSheet.create({
   },
   productContainer: {
     flex: 1,
-    margin: 10,
-    padding: 10,
-    borderWidth: 1,
-    borderColor: '#ccc',
-    borderRadius: 5,
+    marginHorizontal: 5,
+    marginBottom: 20,
+  },
+  imageContainer: {
+    position: 'relative',
+    width: '100%',
     alignItems: 'center',
   },
   productImage: {
     width: '100%',
-    height: 150,
+    height: 200,
     resizeMode: 'contain',
+  },
+  addButton: {
+    position: 'absolute',
+    bottom: 10,
+    right: 10,
   },
   productName: {
     fontSize: 18,
@@ -155,12 +164,12 @@ const styles = StyleSheet.create({
     fontSize: 14,
     color: '#888',
     marginTop: 5,
-    textAlign: 'center',
   },
   productPrice: {
     fontSize: 16,
     fontWeight: 'bold',
     marginTop: 5,
+    color: '#dd8560',
   },
   productList: {
     paddingBottom: 20,
